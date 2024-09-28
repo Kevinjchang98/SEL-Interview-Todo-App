9f6b1710bd7a7f5a6051092358f32ab42baf507d
Kevin Chang <kevinjchang98@gmail.com>
2024-09-28 14:24:13 -0700

    Refactor CSS

--- sel-todo-app/src/app/page.tsx
+++ sel-todo-app/src/app/page.tsx
@@ -1,19 +1,12 @@
-import styles from "./page.module.css";
 import TaskList from "@/components/TaskList/TaskList";
 import ActionMenu from "@/components/ActionMenu/ActionMenu";
 
 export default function Home() {
     return (
-        <div className={styles.page}>
-            <header className={styles.header}>
-                <a href="/">Tasks</a>
-                <a target="_blank" href="https://github.com/Kevinjchang98/SEL-Interview-Todo-App">Source code</a>
-            </header>
-            <main className={styles.main}>
-                <h1>SEL Todo App</h1>
-                <ActionMenu/>
-                <TaskList/>
-            </main>
-        </div>
+        <>
+            <h1>SEL Todo App</h1>
+            <ActionMenu/>
+            <TaskList/>
+        </>
     );
 }
