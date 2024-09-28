7031d0bc2e6c6fde826cade07ac7be8be861f1b3
Kevin Chang <kevinjchang98@gmail.com>
2024-09-28 01:16:43 -0700

    Create flask app and postgresql

---
+++ backend/1-initdb.sql
@@ -0,0 +1,10 @@
+-- Create a database todoapp if one doesn't already exist
+SELECT 'CREATE DATBASE todoapp'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'todoapp')\gexec
+
+CREATE TABLE IF NOT EXISTS tasks
+(
+    id          serial PRIMARY KEY,
+    title       varchar(100) NOT NULL,
+    description varchar(1000)
+);
---
+++ backend/README.md
@@ -0,0 +1,5 @@
+Start with `docker compose up`
+
+`docker compose down --volumes` To delete data
+
+Will create a local Flask server on port 4000 and postgresql on port 5432
