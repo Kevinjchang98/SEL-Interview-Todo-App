eb6759e7b14697bc675df154d19b05da6677ed50
Kevin Chang <kevinjchang98@gmail.com>
2024-09-29 18:18:02 -0700

    Bugfix

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,8 +1,11 @@
 #!bin/bash
 
-apt-get install docker docker-compose nginx
+apt-get install -y docker docker-compose nginx
 
 rm /etc/nginx/sites-available/default
 cp ./nginx-config /etc/nginx/sites-available/default
 
 service nginx restart
+
+docker-compose build
+docker-compose up
