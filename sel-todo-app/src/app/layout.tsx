4e3197510ff41fea0ddccde94208ffe039ae1e52
Kevin Chang <kevinjchang98@gmail.com>
2024-09-29 18:33:12 -0700

    Begin css

--- sel-todo-app/src/app/layout.tsx
+++ sel-todo-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "./nord.css";
 import styles from "@/app/page.module.css";
 
 const geistSans = localFont({
