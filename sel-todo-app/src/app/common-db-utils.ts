69e9553215c9c279d9c9854416884c44aae51c5d
Kevin Chang <kevinjchang98@gmail.com>
2024-10-02 02:04:35 -0700

    Sort tasks by ID

--- sel-todo-app/src/app/common-db-utils.ts
+++ sel-todo-app/src/app/common-db-utils.ts
@@ -1,6 +1,11 @@
+import {TaskArrayTypes} from "@/components/TaskCard/TaskCard";
+
 export async function fetchTaskList() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_DB_HOST}/`, {
     cache: "no-store",
   });
-  return res.json();
+
+  const data = await res.json();
+
+  return data.sort((row: TaskArrayTypes) => row[0])
 }
